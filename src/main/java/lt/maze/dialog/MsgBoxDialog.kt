983f64a7fbdfcbcdf753fac98cdaa1010ff84944
="Justas <justas963@gmail.com>
2016-12-28 22:52:06 +0200

    clickOk renamed to onClickOk for consistency

--- src/main/java/lt/maze/dialog/MsgBoxDialog.kt
+++ src/main/java/lt/maze/dialog/MsgBoxDialog.kt
@@ -34,9 +34,11 @@ open class MsgBoxDialog(player: Player, eventManager: EventManager) : AbstractDi
     open class AbstractMsgBoxDialogBuilder<T, V>(open val dialog: T): AbstractDialog.AbstractDialogBuilder<T, V>(dialog)
         where T: MsgBoxDialog, V: AbstractMsgBoxDialogBuilder<T, V> {
 
+        @Deprecated("Inconsistent", ReplaceWith("onClickOk(handler)"))
+        fun clickOk(handler: (MsgBoxDialog) -> Unit) = onClickOk(handler)
 
         @Suppress("UNCHECKED_CAST")
-        fun clickOk(handler: (MsgBoxDialog) -> Unit): V {
+        fun onClickOk(handler: (MsgBoxDialog) -> Unit): V {
             dialog.clickOkHandler = handler
             return this as V
         }
